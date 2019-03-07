Add async MCTS (disabled right now). Improve MCTS efficiency. Add some chat to agent. Fix removed stones (but seems broken again?). Random rotation/flip for MCTS leaf evaluations. Download sgfs from aya (computer Go server). Train on these sgfs.

mcts.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import itertools
 
 class State:
     def __init__(self):
@@ -23,25 +24,27 @@ class Node:
 from tqdm import tqdm
 
 class MCTS:
-    def __init__(self, exploration_constant, policy_fn, value_fn, iteration_limit = None, time_limit = None):
+    def __init__(self, c_puct, val_policy_fn, iteration_limit = None, time_limit = None):
         self.iteration_limit    = iteration_limit
         self.time_limit         = time_limit # sec
-        self.exploration_constant = exploration_constant
-        self.policy_fn          = policy_fn
-        self.value_fn           = value_fn
+        self.c_puct             = c_puct
+        self.val_policy_fn      = val_policy_fn
         self.root               = None
 
-    def _select_action(self, node, exploration_value):
+    def _select_action(self, node, c_puct):
         def value(action):
+            try:             child = node.children[action]
+            except KeyError: child = Node(node.state, node)
+            try:             p = node.action_probs[action]
+            except KeyError: p = 1 / len(node.state.available_actions)
             try:
-                child = node.children[action]
                 Q = child.total_reward / child.num_visits
-                U = exploration_value * \
-                    node.action_probs[action] * np.sqrt(node.num_visits) / (1+child.num_visits)
-            except KeyError:
+            except ZeroDivisionError:
                 Q = 0.5
-                U = exploration_value * \
-                    node.action_probs[action] * np.sqrt(node.num_visits)
+            try:
+                U = c_puct * p * np.sqrt(node.num_visits) / (1+child.num_visits)
+            except (KeyError, ZeroDivisionError):
+                U = c_puct * p * np.sqrt(node.num_visits) / (1+child.num_visits)
             if node.state.turn == -1: Q = 1 - Q
             return Q + U
         action_values = {
@@ -52,7 +55,7 @@ class MCTS:
     def _select_leaf_node(self, node):
         if node.state.terminal:
             return node
-        action = self._select_action(node, self.exploration_constant)
+        action = self._select_action(node, self.c_puct)
         if action not in node.children:
             new_state = node.state.step(action)
             child = Node(new_state, node)
@@ -66,27 +69,31 @@ class MCTS:
         node.total_reward += reward
         self._backpropagate(node.parent, reward)
 
-    def _evaluate(self, node):
-        value, action_probs = self.value_fn(node.state), self.policy_fn(node.state)
+    def _evaluate(self, nodes):
+        value, action_probs = self.val_policy_fn([node.state for node in nodes])
         return value, action_probs
 
     def run_simulation(self, iteration_limit=None, time_limit=None):
-        start_time = time.time()
-
-        for _ in range(iteration_limit) if iteration_limit else iter(int, 1):
-            if time_limit and time.time() - start_time > time_limit: break
+        start_time      = time.time()
+        eval_batch_size = 1
+        updates         = 0
+        while True:
+            updates += eval_batch_size
             # 1. Selection
-            leaf                = self._select_leaf_node(self.root)
+            leafs                   = [self._select_leaf_node(self.root) for _ in range(eval_batch_size)]
             # 2. Evaluation
-            value, action_probs = self._evaluate(leaf)
-            leaf.action_probs   = action_probs
-            # 3. Backpropagation
-            self._backpropagate(leaf, value)
-
-            if time_limit is None and iteration_limit is None:
+            vals, pis               = self._evaluate(leafs)
+            for leaf, val, pi in zip(leafs, vals, pis):
+                leaf.action_probs   = pi
+                # 3. Backpropagation
+                self._backpropagate(leaf, val)
+            if (time_limit is None or time.time() - start_time > time_limit) and (iteration_limit is None or updates > iteration_limit):
                 break
+        # print('%d iter in %.2f sec = %.2f iter/sec' % (
+        #     updates, time.time() - start_time, updates / (time.time() - start_time)
+        # ))
 
-    def get_action(self, state):
+    def get_action(self, state, update_root=True):
         try:
             # Update Root
             prev_action = state.prev_action
@@ -99,16 +106,17 @@ class MCTS:
             # print()
             # Create New Tree
             self.root = Node(state, None)
-            value, action_probs = self._evaluate(self.root)
-            self.root.action_probs = action_probs
-            self._backpropagate(self.root, value)
+            value, action_probs = self._evaluate([self.root])
+            self.root.action_probs = action_probs[0]
+            self._backpropagate(self.root, value[0])
 
         # Run Simulation
         self.run_simulation(self.iteration_limit, self.time_limit)
 
         # Select Action
         action = max(self.root.children, key=lambda action: self.root.children[action].num_visits)
-        try:    self.root = self.root.children[action]; #print("VALUE: %s" % (self.root.total_reward / self.root.num_visits))
-        except: pass
+        if update_root:
+            try:    self.root = self.root.children[action]; #print("VALUE: %s" % (self.root.total_reward / self.root.num_visits))
+            except: pass
         
         return action

ogs_api.py
@@ -3,7 +3,6 @@
 import numpy as np
 import requests
 import websocket
-import threading
 import json
 import time
 import logging
@@ -11,6 +10,8 @@ import traceback
 import yaml
 import uuid
 import threading
+import multiprocessing
+import random
 
 from go import Go
 from players import Player, MCTS_Player, PolicyNetworkArgmax
@@ -205,7 +206,23 @@ class OnlineGoAPI:
         )
 
     def _game_chat(self, game_id, message):
-        pass # TODO
+        self.logger.info('_game_chat %s %s' % (game_id, message))
+        self._ws_send(
+            'game/chat',
+            {  
+                'auth': self.config['chat_auth'],
+                'game_id': game_id,
+                'player_id': str(self.config['user']['id']),
+                'body': message,
+                'type': "discussion",
+                'move_number': len(self.gamedata[game_id]['moves']),
+                'username': self.username,
+                'is_player': True,
+                'ranking': self.gamedata[game_id]['players']['black']['rank'] if int(self.gamedata[game_id]['black_player_id']) == int(self.config['user']['id']) else self.gamedata[game_id]['players']['white']['rank'],
+                'ui_class': "",
+            }
+        )
+
     def _game_connect(self, game_id):
         self.logger.info('_game_connect %s' % game_id)
         self._ws_send('game/connect', {'player_id': str(self.config['user']['id']), 'game_id': str(game_id), 'chat': True})
@@ -237,7 +254,7 @@ class OnlineGoAPI:
                 'player_id': str(self.config['user']['id']),
             }
         )
-    def _removed_stones_accept(self, game_id):
+    def _removed_stones_accept(self, game_id, stones):
         self.logger.info('_removed_stones_accept %s' % game_id)
         self._ws_send(
             ' game/removed_stones/accept',
@@ -245,7 +262,7 @@ class OnlineGoAPI:
                 'auth': self.config['chat_auth'],
                 'game_id': str(game_id),
                 'player_id': str(self.config['user']['id']),
-                'stones': self.gamedata[game_id]['removed'],
+                'stones': stones,
                 'strict_seki_mode': self.gamedata[game_id]['strict_seki_mode'],
             }
         )
@@ -284,24 +301,22 @@ class OnlineGoAPI:
                 self.gamedata[game_id]['clock'] = data
             elif game_endpoint == 'phase':
                 self.gamedata[game_id]['phase'] = data
-                if data == 'stone removal':
-                    self._game_chat(game_id, "I dunno how to do this bit")
-                elif data == 'play':
+                if data == 'play':
                     del self.gamedata[game_id]
                     self._game_disconnect(game_id)
                     self._game_connect(game_id)
             elif game_endpoint == 'move':
                 self.gamedata[game_id]['moves'].append(data['move'])
             elif game_endpoint == 'removed_stones':
-                # TODO
-                self.logger.debug('removed_stones %s' % game_id)
-                self.logger.debug(str(data))
                 pass
+            elif game_endpoint == 'removed_stones_accepted':
+                if int(data['player_id']) != int(self.config['user']['id']):
+                    time.sleep(1)
+                    self._removed_stones_accept(game_id, data['stones'])
             elif game_endpoint == 'reset':
                 self.logger.warning('game/reset %s' % game_id)
                 del self.gamedata[game_id]
                 self._game_connect(game_id)
-
 
     def _ws_on_open(self):
         self.ws_log.info(' O  on_open')
@@ -314,7 +329,7 @@ class OnlineGoAPI:
         msg = json.loads(message[2:])
         self._process_message(msg)
     def _ws_send(self, endpoint, message):
-        msg = '42'+json.dumps([endpoint, message]).replace(' ','')
+        msg = '42'+json.dumps([endpoint, message], separators=(',',':'))
         self.ws_log.info(' >  %s' % msg)
         self.ws.send(msg)
 
@@ -333,21 +348,57 @@ class OnlineGoAPI:
         return res
 
 def run(game_id, ogs_api, player, logger):
+    def _start_msg():
+        msgs = [
+            'glhf!',
+            'hey :)',
+            'good luck!',
+            'have fun',
+            'hi!',
+            'hello',
+            'hi! glhf',
+        ]
+        return random.choice(msgs)
+    def _end_msg():
+        msgs = [
+            'gg',
+            'good game',
+            'gg thanks',
+            'thx',
+        ]
+        return random.choice(msgs)
+    def _losing_msg():
+        msgs = [
+            'ow',
+            'oops',
+            ':(',
+        ]
+        return random.choice(msgs)
+    def _winning_msg():
+        msgs = [
+            'ye',
+            ':)',
+            'hehe',
+        ]
+        return random.choice(msgs)
     def gamedata():
         return ogs_api.gamedata[game_id]
+    def _move_count():
+        return len(gamedata()['moves'])
+    def _playing_black():
+        return int(gamedata()['clock']['black_player_id']) == int(ogs_api.config['user']['id'])
     def _game_over():
         return gamedata()['phase'] == 'finished'
     def _my_turn():
         return gamedata()['phase'] == 'play' and \
-            gamedata()['clock']['current_player'] == ogs_api.config['user']['id']
+            int(gamedata()['clock']['current_player']) == int(ogs_api.config['user']['id'])
     def _time_left():
-        clock           = gamedata()['clock']
-        time_left       = clock['black_time'] if clock['black_player_id'] == ogs_api.config['user']['id'] else clock['white_time']
+        time_left       = gamedata()['clock']['black_time'] if _playing_black() else gamedata()['clock']['white_time']
         total_time      = time_left['thinking_time']
         try: total_time += time_left['periods'] * time_left['period_time']
         except: pass
         if _my_turn():
-            time_elapsed = (time.time() - clock['last_move'] / 1000)
+            time_elapsed = (time.time() - gamedata()['clock']['last_move'] / 1000)
             total_time  -= time_elapsed
         return total_time
     def _get_move_time():
@@ -370,15 +421,29 @@ def run(game_id, ogs_api, player, logger):
             logger.warning('%s : %s' % (game_id, traceback.format_exc()))
             move = Go.PASS
         return move
+    def _win_prob(go):
+        black_win_prob = player.black_win_prob(go)
+        return black_win_prob if _playing_black() else 1-black_win_prob
     def _do_turn():
         logger.info('%s : _do_turn' % game_id)
         go   = _get_go()
-        mt   = _get_move_time()
-        move = _get_move(go, mt)
-        logger.info('%s : move %s' % (game_id, move))
-        ogs_api._game_move(game_id, move)
-        logger.info('%s : move submitted' % (game_id))
+        win_prob = _win_prob(go)
+        win_probs.append(win_prob)
+        logger.info('%s : win_prob=%.2f' % (game_id, win_prob))
+        if len(win_probs) > 3 and all([wp < 0.05 for wp in win_probs[-3:]]):
+            logger.info('%s : resign' % (game_id))
+            ogs_api._game_resign(game_id)
+        else:
+            mt   = _get_move_time()
+            move = _get_move(go, mt)
+            logger.info('%s : move %s' % (game_id, move))
+            ogs_api._game_move(game_id, move)
     logger.info('%s : start' % (game_id))
+    win_probs = []
+    sent_msgs = [0]
+    if _move_count() < 2:
+        ogs_api._game_chat(game_id, _start_msg())
+        sent_msgs.append(_move_count())
     while not _game_over():
         logger.info('%s : sleep' % (game_id))
         while not _my_turn() and not _game_over():
@@ -387,15 +452,25 @@ def run(game_id, ogs_api, player, logger):
         logger.info('%s : wake' % (game_id))
         if _game_over():
             logger.info('%s : game over' % (game_id))
+            ogs_api._game_chat(game_id, _end_msg())
+            sent_msgs.append(_move_count())
             return
         _do_turn()
         time.sleep(0.5)
+        if _move_count() - sent_msgs[-1] > 10 and len(win_probs) > 4:
+            delta_wp = win_probs[-1] - np.mean(win_probs[-5:-1])
+            if delta_wp > 0.4 and win_probs[-1] > 0.6:
+                ogs_api._game_chat(game_id, _winning_msg())
+                sent_msgs.append(_move_count())
+            if delta_wp < -0.4 and win_probs[-1] < 0.4:
+                ogs_api._game_chat(game_id, _losing_msg())
+                sent_msgs.append(_move_count())
 
 if __name__ == '__main__':
     logger  = make_logger('debug')
     model  = Gen_Model().load('Go', '0.1')
     def new_player():
-        return MCTS_Player(model, time_limit=30)
+        return MCTS_Player(model, time_limit=10)
     # Initialise TF session
     MCTS_Player(model, time_limit=1).get_move(Go())
     max_concurrent_games = 2
@@ -412,6 +487,7 @@ if __name__ == '__main__':
                 logger.info("start thread for %s" % game_id)
                 # run(game_id, ogs_api, new_player())
                 threading.Thread(target=run, args=(game_id, ogs_api, new_player(), logger)).start()
+                # multiprocessing.Process(target=run, args=(game_id, ogs_api, new_player(), logger)).start()
                 run_threads[game_id] = True
         if ogs_api.automatch is None and len(play_games) < max_concurrent_games:
             logger.info("FIND AUTOMATCH")

players.py
@@ -2,6 +2,7 @@ from joblib import Parallel, delayed
 import itertools
 import numpy as np
 import traceback
+import random
 
 from go import Go
 
@@ -149,15 +150,46 @@ class MCTS_Player:
     def __init__(self, model, time_limit=1):
         self.name = 'MCTS_Player'
         self.model = model
-        preprocess_input = lambda gostate: single_go_to_x(gostate.go)
-        value_fn  = lambda gostate: self.model.predict(preprocess_input(gostate))[0][0]
-        policy_fn = lambda gostate: self.model.predict(preprocess_input(gostate))[1][0]
+        translations = [
+            lambda x: x,
+            lambda x: np.rot90(x, k=1, axes=(1,2)),
+            lambda x: np.rot90(x, k=2, axes=(1,2)),
+            lambda x: np.rot90(x, k=3, axes=(1,2)),
+            lambda x: np.flip(x, axis=1),
+            lambda x: np.rot90(np.flip(x, axis=1), k=1, axes=(1,2)),
+            lambda x: np.rot90(np.flip(x, axis=1), k=2, axes=(1,2)),
+            lambda x: np.rot90(np.flip(x, axis=1), k=3, axes=(1,2)),
+        ]
+        inv_translations = [
+            lambda x: x,
+            lambda x: np.rot90(x, k=-1, axes=(1,2)),
+            lambda x: np.rot90(x, k=-2, axes=(1,2)),
+            lambda x: np.rot90(x, k=-3, axes=(1,2)),
+            lambda x: np.flip(x, axis=1),
+            lambda x: np.flip(np.rot90(x, k=-1, axes=(1,2)), axis=1),
+            lambda x: np.flip(np.rot90(x, k=-2, axes=(1,2)), axis=1),
+            lambda x: np.flip(np.rot90(x, k=-3, axes=(1,2)), axis=1),
+        ]
+        def val_policy_fn(gostates):
+            x = np.concatenate(
+                [single_go_to_x(gostate.go) for gostate in gostates],
+                axis=0
+            )
+            idx = random.choice(range(len(translations)))
+            x_trans = translations[idx](x)
+            val, pi_trans = self.model.predict(x_trans)
+            pi_moves_trans = pi_trans[:,:-1].reshape(-1,9,9,1)
+            pi_moves = inv_translations[idx](pi_moves_trans)
+            pi = np.append(pi_moves.reshape(pi_trans[:,:-1].shape), pi_trans[:,-1:], axis=1)
+            return val, pi
         self.mcts = MCTS(
-            exploration_constant = 0.1,
-            policy_fn = policy_fn,
-            value_fn  = value_fn,
+            c_puct = 0.1,
+            val_policy_fn = val_policy_fn,
             time_limit = time_limit,
         )
+    def black_win_prob(self, go):
+        val, pi = self.mcts.val_policy_fn([MCTS_Player.GoState(go)])
+        return val[0]
     def get_move(self, go):
         state = MCTS_Player.GoState(go)
         return state.action_to_gomove(self.mcts.get_action(state))

train.py
@@ -1,14 +1,23 @@
 
+#%%
 import os
+import tarfile
+
+import codecs
+from tqdm import tqdm
 
 import numpy as np
+
+import h5py
+
 from go import Go
 
 import functools as ft
 
 import traceback
+import random
 
-from sgf_converter import play_sgf
+from sgf_converter import play_sgf, WR, BR
 
 from tensorflow.keras.utils import Sequence
 
@@ -50,73 +59,160 @@ def single_go_to_x(go):
         np.ones(shape=(1,9,9,1))*(go._turn == Go.BLACK),
     ], axis=3)
 
-class DataGenerator(Sequence):
-    def __init__(self, sgfs, batch_size):
-        self.sgfs       = sgfs
-        self.batch_size = batch_size
-        self.data       = self._prepare_data(sgfs)
-    def _prepare_data(self, sgfs):
-        def process_sgf(sgf):
-            game = play_sgf(sgf)
-            def full_go_to_x(go):
-                if go._prev is not None:
-                    return np.concatenate(
-                        [full_go_to_x(go._prev), single_go_to_x(go)],
-                        axis=0
-                    )
-                return single_go_to_x(go)
-            def full_go_to_y_policy(go):
-                reversed_moves = [Go.PASS]
-                while go._prev is not None:
-                    reversed_moves.append(go._last_move)
-                    go = go._prev
-                ys = np.zeros(shape=(len(reversed_moves),go._size*go._size+1))
-                for i,move in enumerate(reversed(reversed_moves)):
-                    if move != Go.PASS:
-                        idx = np.ravel_multi_index(move, (9,9))
-                        ys[i][idx] = 1
-                    else:
-                        ys[i][-1] = 1
-                return ys
-            go = game['board']
-            winner = game['RE']
-            x = full_go_to_x(go)
-            y_value = np.full(
-                shape=(len(x),1), 
-                fill_value=1 if winner[0] == 'B' else 0
-            )
-            y_policy = full_go_to_y_policy(go)
-            return x,y_value,y_policy
-        return Parallel(n_jobs=4)(
-            delayed(process_sgf)(sgf) for sgf in tqdm(sgfs, ncols=80)
+def get_x(go):
+    if go._prev is not None:
+        return np.concatenate(
+            [get_x(go._prev), single_go_to_x(go)],
+            axis=0
         )
-    def __len__(self):
-        return int(np.ceil(len(self.sgfs) / float(self.batch_size)))
-    #@ft.lru_cache(maxsize=None)
-    def __getitem__(self, idx):
-        batch_data  = self.data[idx * self.batch_size:(idx + 1) * self.batch_size]
-        xs, y_values, y_policies = [], [], []
-        for x,y,yy in batch_data:
-            idx = np.random.randint(0, len(x))
-            xs.append(x[idx])
-            y_values.append(y[idx])
-            y_policies.append(yy[idx])
-        return np.stack(xs, axis=0), {'value_head': np.stack(y_values, axis=0), 'policy_head': np.stack(y_policies, axis=0)}
+    return single_go_to_x(go)
+
+def get_y_policy(go):
+    reversed_moves = [Go.PASS]
+    while go._prev is not None:
+        reversed_moves.append(go._last_move)
+        go = go._prev
+    y_policy = np.zeros(shape=(len(reversed_moves),go._size*go._size+1))
+    for i,move in enumerate(reversed(reversed_moves)):
+        if move != Go.PASS:
+            idx = np.ravel_multi_index(move, (go._size, go._size))
+            y_policy[i][idx] = 1
+        else:
+            y_policy[i][-1] = 1
+    return y_policy
+
+def get_y_value(winner, n):
+    y_value = np.full(
+        shape=(n,1), 
+        fill_value=1 if winner[0] == 'B' else 0
+    )
+    return y_value
+
+def process_sgf(sgf):
+    game = play_sgf(sgf)
+    go = game['board']
+    winner = game['RE']
+    x           = get_x(go)
+    y_policy    = get_y_policy(go)
+    y_value     = get_y_value(winner, len(x))
+    return x, y_policy, y_value
+
+def get_sgfs():
+    datasets = os.listdir('data/aya')
+    for d in sorted(datasets, reverse=True): # most recent first
+        with tarfile.open('data/aya/%s' % d, 'r:bz2') as tar:
+            for member in tar.getmembers():
+                if member.isfile():
+                    f = tar.extractfile(member)
+                    content = f.read()
+                    yield content.decode('utf-8')
+
+def len_sgfs():
+    # 187316 for 9x9_2015_11 -> 9x9_2018_12
+    datasets = os.listdir('data/aya')
+    total_len = 0
+    for d in datasets:
+        with tarfile.open('data/aya/%s' % d, 'r:bz2') as tar:
+            total_len +=  len([m for m in tar.getmembers() if m.isfile()])
+    return total_len
 
 if __name__ == '__main__':
+    # n_games = len_sgfs()
+    mem_size = 100000
+
     # SUPERVISED LEARNING
+    sgfs = get_sgfs()
+    demo_sgf = sgfs.__next__()
+    x, y_policy, y_value = process_sgf(demo_sgf)
+
+    try:
+        x_memory = np.memmap(
+            filename = 'memory/x.dat',
+            dtype = np.uint8, mode='r+',
+            shape=(mem_size,)+x.shape[1:]
+        )
+    except FileNotFoundError:
+        x_memory = np.memmap(
+            filename = 'memory/x.dat',
+            dtype = np.uint8, mode='w+',
+            shape=(mem_size,)+x.shape[1:]
+        )
+        del x_memory
+        x_memory = np.memmap(
+            filename = 'memory/x.dat',
+            dtype = np.uint8, mode='r+',
+            shape=(mem_size,)+x.shape[1:]
+        )
+    try:
+        y_value_memory = np.memmap(
+            filename = 'memory/y_value.dat',
+            dtype = np.uint8, mode='r+',
+            shape=(mem_size,)+y_value.shape[1:]
+        )
+    except FileNotFoundError:
+        y_value_memory = np.memmap(
+            filename = 'memory/y_value.dat',
+            dtype = np.uint8, mode='w+',
+            shape=(mem_size,)+y_value.shape[1:]
+        )
+        del y_value_memory
+        y_value_memory = np.memmap(
+            filename = 'memory/y_value.dat',
+            dtype = np.uint8, mode='r+',
+            shape=(mem_size,)+y_value.shape[1:]
+        )
+    try:
+        y_policy_memory = np.memmap(
+            filename = 'memory/y_policy.dat',
+            dtype = np.uint8, mode='r+',
+            shape=(mem_size,)+y_policy.shape[1:]
+        )
+    except FileNotFoundError:
+        y_policy_memory = np.memmap(
+            filename = 'memory/y_policy.dat',
+            dtype = np.uint8, mode='w+',
+            shape=(mem_size,)+y_policy.shape[1:]
+        )
+        del y_policy_memory
+        y_policy_memory = np.memmap(
+            filename = 'memory/y_policy.dat',
+            dtype = np.uint8, mode='r+',
+            shape=(mem_size,)+y_policy.shape[1:]
+        )
 
     # Load Data
-    import codecs
-    from tqdm import tqdm
-    sgfs = []
-    path = 'sgfs/sdk_9x9/'
-    n_games = len(os.listdir(path))
-    for game_id in tqdm(os.listdir(path)[:n_games], ncols=80):
-        sgfs.append(codecs.open('sgfs/sdk_9x9/%s' % game_id, 'r', 'utf-8').read())
-
-    # Sequence Generator for Training
-    data_generator = DataGenerator(sgfs, batch_size = 128)
+    def to_int(s):
+        return int(''.join(c for c in s if c.isdigit()))
+    ratings     = []
+    print("Loading data into memory...")
+    idx_start   = 0
+    n_games     = 0
+    tot_games   = 0
+    with tqdm(
+        total=mem_size, ncols=120,
+        postfix=['games', dict(value=0)]
+    ) as pbar:
+        for i,sgf in enumerate(sgfs):
+            if idx_start >= mem_size: break
+            # Ignore games not in top 10% of ratings
+            wr,br = to_int(WR(sgf)), to_int(BR(sgf))
+            ratings += [wr,br]
+            if len(ratings) < 100 or min(wr,br) < np.percentile(ratings[-1000:], 90):
+                continue
+            x, y_policy, y_value = process_sgf(sgf)
+            idx_end = idx_start + len(x)
+            if idx_end > mem_size:
+                x        = x[:mem_size - idx_end]
+                y_policy = y_policy[:mem_size - idx_end]
+                y_value  = y_value[:mem_size - idx_end]
+                idx_end = mem_size
+            x_memory[idx_start:idx_end]         = x
+            y_policy_memory[idx_start:idx_end]  = y_policy
+            y_value_memory[idx_start:idx_end]   = y_value
+            idx_start   += len(x)
+            n_games     += 1
+            pbar.update(len(x))
+            pbar.set_postfix(game=n_games, tot_games=i+1)
 
     # Build Model
     model = Residual_CNN(
@@ -133,12 +229,16 @@ if __name__ == '__main__':
     model.model.compile(
         loss={'value_head': 'binary_crossentropy', 'policy_head': 'categorical_crossentropy'},#softmax_cross_entropy_with_logits},
         optimizer='adam',
-        loss_weights={'value_head': 0.5, 'policy_head': 0.5},
+        loss_weights={'value_head': 0.2, 'policy_head': 0.8},
         metrics={'value_head': 'accuracy', 'policy_head': 'accuracy'},
-    )
-    model.model.fit_generator(
-        generator = data_generator,
-        epochs = 100, verbose = 1,
+    ) # states, targets, epochs, verbose, validation_split, batch_size
+    model.model.fit(
+        x_memory[:idx_end], 
+        {
+            'value_head'    : y_value_memory[:idx_end], 
+            'policy_head'   : y_policy_memory[:idx_end],
+        },
+        epochs=1, verbose=1, batch_size=64, shuffle=True,
     )
 
     # Save Model